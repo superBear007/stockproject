14d2249e24b8e1f258958793cd37d5badfc9e0f3
yanchao <yanchaowhu@163.com>
2019-09-16 17:15:35 +0800

    框架设计

--- getDataFromTushare.py
+++ getDataFromTushare.py
@@ -1,7 +1,13 @@
 import tushare as ts
 
-pro = ts.pro_api("5426ad82176097db40ef42f021474c04bc6b7ab57c520d8dcc4213b3")
 
+class TsData():
 ## 获取股票列表
-def get_basic_info():
-   return ts.get_stock_basics()
+   def __init__(self):
+      self.pro = ts.pro_api("5426ad82176097db40ef42f021474c04bc6b7ab57c520d8dcc4213b3")
+
+   def get_basic_info(self):
+      return self.pro.stock_basic()
+
+   def get_daily(self,date):
+      return self.pro.daily(trade_date=date)
--- stockBasicUpdate.py
+++ stockBasicUpdate.py
@@ -1,2 +1,12 @@
+from stockproject.config_settings import *
+import tushare as ts
+from stockproject.getDataFromTushare import TsData
+
+
 def stockBasic():
+
     pass
+
+
+data = TsData()
+print(data.get_basic_info())
